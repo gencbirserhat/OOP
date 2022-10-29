54b27f0da4ef5c6685a8385ee4cbf0146a48430d
gencbirserhat <78283289+gencbirserhat@users.noreply.github.com>
2022-10-29 17:31:01 +0300

    rework

---
+++ OOP.Interfaces.ICloneable/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Interfaces.ICloneables
+{
+    public class Book : ICloneable
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public double Price { get; set; }
+
+        
+        
+        public object Clone()
+        {
+            // a deep copy
+            return new Book()
+            {
+                Id = this.Id,
+                Title = this.Title,
+                Price = this.Price
+            }; 
+
+            // shallow copy (alternative)
+            //return this.MemberwiseClone(); 
+        }
+    }
+}
---
+++ OOP.Interfaces.ICloneable/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace OOP.Interfaces.ICloneables
+{
+    class Program
+    {
+        static void Main()
+        {
+            var book1 = new Book()
+            {
+                Id = 1,
+                Title= "Suç ve Ceza",
+                Price = 25.5
+            };
+            var book2 = (Book)book1.Clone();
+            book2.Id = 2;
+            book2.Title = "Sefiller";
+            book2.Price = 30;
+            Console.WriteLine(book1.Id + " " + book1.Title);
+            Console.WriteLine(book2.Id + " " + book2.Title);
+
+            Console.ReadKey();
+        }
+    }
+}
