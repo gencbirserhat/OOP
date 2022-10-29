a23801a47ce025db9eb09413ff233facc40ce16f
gencbirserhat <78283289+gencbirserhat@users.noreply.github.com>
2022-10-29 18:59:13 +0300

    rework

---
+++ OOP.Interfaces.IComparables/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace OOP.Interfaces.IComparables
+{
+    class Program
+    {
+        static void Main()
+        {
+            var employees = new List<Employee>()
+            {
+                new Employee()
+                {
+                    Id = 1,
+                    FullName = "Hünkar Acar",
+                    Salary= 1000000
+                },
+                new Employee()
+                {
+                    Id= 2,
+                    FullName = "Serhat Genç",
+                    Salary = 500
+                    
+                },
+                new Employee()
+                {
+                    Id= 3,
+                    FullName = "Merve Dağ",
+                    Salary = 30000
+
+                },
+                new Employee()
+                {
+                    Id= 4,
+                    FullName = "Yağız İpek",
+                    Salary = 7000
+
+                },
+                new Employee()
+                {
+                    Id= 5,
+                    FullName = "Han Taşçı",
+                    Salary = 5500
+
+                },
+                new Employee()
+                {
+                    Id= 6,
+                    FullName = "Mehmet Taşçı",
+                    Salary = 15500
+
+                }
+
+            };
+            employees.Sort();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
---
+++ OOP.Interfaces.IComparablesT/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Interfaces.IComparablesT
+{
+    public class Employee: IComparable<Employee>
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public decimal Salary { get; set; }
+
+        public int CompareTo(Employee? other)
+        {
+            //return this.Salary.CompareTo(other?.Salary);
+            if (this.Salary < other?.Salary)
+            {
+                return 1;
+                
+            }
+            else if (this.Salary.Equals(other?.Salary))
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Id,-5}{FullName,-25}{Salary,-7}";
+        }
+    }
+
+}
